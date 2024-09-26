d8482ffd484a0838c78d8eb72d321c4f07e05cef
Vansh <v3wahi@uwaterloo.ca>
2024-09-26 14:19:25 -0400

    projects tile correct

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Slider from 'react-slick';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'; // Font Awesome Icons
+import { Swiper, SwiperSlide } from 'swiper/react'; // Import Swiper React components
+import { Navigation, Pagination } from 'swiper/modules'; // Correct import for Swiper 8+
+import 'swiper/css'; // Import Swiper styles
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './Projects.css';
-
-// Custom Next Arrow Component
-const NextArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className={`${className} custom-arrow next-arrow`} onClick={onClick}>
-      <FontAwesomeIcon icon={faChevronRight} />
-    </div>
-  );
-};
-
-// Custom Previous Arrow Component
-const PrevArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className={`${className} custom-arrow prev-arrow`} onClick={onClick}>
-      <FontAwesomeIcon icon={faChevronLeft} />
-    </div>
-  );
-};
 
 const Projects = () => {
   const projects = [
@@ -52,47 +34,37 @@ const Projects = () => {
     },
   ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />, // Use custom next arrow
-    prevArrow: <PrevArrow />, // Use custom prev arrow
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="projects-container">
       <h2 className="projects-title">My Projects</h2>
-      <Slider {...settings}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={30}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1024: {
+            slidesPerView: 2,
+          },
+          600: {
+            slidesPerView: 1,
+          },
+        }}
+      >
         {projects.map((project, index) => (
-          <div key={index} className="project-tile">
-            <img src={project.image} alt={project.title} className="project-thumbnail" />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
-              View Project
-            </a>
-          </div>
+          <SwiperSlide key={index}>
+            <div className="project-tile">
+              <img src={project.image} alt={project.title} className="project-thumbnail" />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
+                View Project
+              </a>
+            </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 };
